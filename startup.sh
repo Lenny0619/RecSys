788269606e29f126484d63f1b8029413fcdd11cd
LENNY TANG TUNG EING <lennytang0619@gmail.com>
2025-06-05 00:27:28 +0800

    update

--- startup.sh
+++ startup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 cd /home/site/wwwroot/RecSys
 source /home/site/wwwroot/venv/bin/activate
+
+# Log installed packages for debugging
+pip list | tee /home/logs/packages.log
+
 exec gunicorn --bind 0.0.0.0:8000 --timeout 120 --workers 2 app:app

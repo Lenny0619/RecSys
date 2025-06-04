6abc4476a21c1777e9bfaf6fcbeccd22fc5f66df
LENNY TANG TUNG EING <lennytang0619@gmail.com>
2025-06-04 23:35:03 +0800

    update

--- startup.sh
+++ startup.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# Navigate to application folder
 cd /home/site/wwwroot/RecSys
-
-# Activate venv (if needed)
 source /home/site/wwwroot/venv/bin/activate
-
-# Start Gunicorn with correct app reference
-exec gunicorn --bind 0.0.0.0:8000 --timeout 120 --workers 2 RecSys.app:app
+exec gunicorn --bind 0.0.0.0:8000 --timeout 120 --workers 2 app:app
